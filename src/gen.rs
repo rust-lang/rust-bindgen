rust-lang/rust-bindgen

commit fa6f4407f3cd366c2390b390a6400db8a5c5a071

produce valid univariant enums
Instead of generating a enum like this:

```
pub enum Enum_CUipcMem_flags_enum {
    CU_IPC_MEM_LAZY_ENABLE_PEER_ACCESS = 1,
}
```

Which is a compiler error because repr can't be used with univariant enums.
See https://github.com/rust-lang/rust/issues/10292

We generate a two variant enum with a dummy variant.

```
pub enum Enum_CUipcMem_flags_enum {
    CU_IPC_MEM_LAZY_ENABLE_PEER_ACCESS = 1,
    __DUMMY,
}
```

Which is the recommend workaround to the previous compiler error.

closes #255

src/gen.rs
@@ -745,6 +745,16 @@ fn cenum_to_rs(ctx: &mut GenCtx, name: String, kind: IKind, enum_items: &[EnumIt
         }));
     }
 
+    // for univariant enums, we add a dummy variant to avoid rust-lang/rust#10292
+    if enum_items.len() == 1 {
+        variants.push(P(respan(ctx.span, ast::Variant_ {
+            name: ctx.ext_cx.ident_of("__DUMMY"),
+            attrs: vec![],
+            data: ast::VariantData::Unit(ast::DUMMY_NODE_ID),
+            disr_expr: None,
+        })));
+    }
+
     let enum_repr = InternedString::new(enum_kind_to_rust_type_name(kind));
 
     let repr_arg = ctx.ext_cx.meta_word(ctx.span, enum_repr);

commit c77afae06343884a411ef2ed72d78b828f16371f

Fix typo introduced by fa6f440.

src/gen.rs
@@ -758,12 +758,12 @@ fn cenum_to_rs(ctx: &mut GenCtx, name: String, kind: IKind, enum_items: &[EnumIt
 
     // for univariant enums, we add a dummy variant to avoid rust-lang/rust#10292
     if enum_items.len() == 1 {
-        variants.push(P(respan(ctx.span, ast::Variant_ {
+        variants.push(respan(ctx.span, ast::Variant_ {
             name: ctx.ext_cx.ident_of("__DUMMY"),
             attrs: vec![],
             data: ast::VariantData::Unit(ast::DUMMY_NODE_ID),
             disr_expr: None,
-        })));
+        }));
     }
 
     let enum_repr = InternedString::new(enum_kind_to_rust_type_name(kind));