800289101a97c2677b1282c13f6e98a28a65280e
Yamakaky <yamakaky@yamaworld.fr>
2016-05-27 19:38:51 +0200

    Add missing generated module attribute.

--- src/gen.rs
+++ src/gen.rs
@@ -269,6 +269,7 @@ pub fn gen_mod(
                       "allow",
                       &["dead_code",
                         "non_camel_case_types",
+                        "non_upper_case_globals",
                         "non_snake_case"],
                       ast::AttrStyle::Inner)
     ];
