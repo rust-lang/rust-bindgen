9a53425480a37d5e75c96eda6bb9144eb2d438c2
Jyun-Yan You <jyyou.tw@gmail.com>
2016-05-29 16:33:42 +0800

    Auto-build docs

---
+++ scripts/travis-doc-upload.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# License: CC0 1.0 Universal
+# https://creativecommons.org/publicdomain/zero/1.0/legalcode
+
+set -e
+
+PROJECT_NAME=bindgen
+SSH_KEY_TRAVIS_ID=26066e6f8a7a
+
+[ "$TRAVIS_RUST_VERSION" = stable ]
+
+[ "$TRAVIS_BRANCH" = master ]
+
+[ "$TRAVIS_PULL_REQUEST" = false ]
+
+eval key=\$encrypted_${SSH_KEY_TRAVIS_ID}_key
+eval iv=\$encrypted_${SSH_KEY_TRAVIS_ID}_iv
+
+mkdir -p ~/.ssh
+openssl aes-256-cbc -K $key -iv $iv -in scripts/travis-doc.key.enc -out ~/.ssh/id_rsa -d
+chmod 600 ~/.ssh/id_rsa
+
+cargo doc
+echo "<meta http-equiv=refresh content=0;url=`echo $PROJECT_NAME`/index.html>" > target/doc/index.html
+
+sudo pip install ghp-import
+ghp-import -n target/doc
+
+git config user.name "doc upload bot"
+git config user.email "nobody@example.com"
+git push -fq origin gh-pages
