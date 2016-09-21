6ed0253913afc0c0258eaedfb27d87322250f2b2
Emilio Cobos Álvarez <ecoal95@gmail.com>
2016-09-21 23:16:05 +0200

    Index unnamed types by canonical declaration.
    This fixes union_with_nesting.h

--- tests/expectations/struct_with_nesting.rs
+++ tests/expectations/struct_with_nesting.rs
@@ -35,7 +35,7 @@ pub struct foo {
 pub struct foo__bindgen_ty_bindgen_id_4 {
     pub b: __BindgenUnionField<::std::os::raw::c_uint>,
     pub __bindgen_anon_1: __BindgenUnionField<foo__bindgen_ty_bindgen_id_4__bindgen_ty_bindgen_id_7>,
-    pub __bindgen_anon_2: __BindgenUnionField<foo__bindgen_ty_bindgen_id_4__bindgen_ty_bindgen_id_7>,
+    pub __bindgen_anon_2: __BindgenUnionField<foo__bindgen_ty_bindgen_id_4__bindgen_ty_bindgen_id_12>,
     pub bindgen_union_field: u32,
 }
 #[repr(C)]
@@ -52,6 +52,24 @@ fn bindgen_test_layout_foo__bindgen_ty_bindgen_id_4__bindgen_ty_bindgen_id_7() {
                , 2usize);
 }
 impl Clone for foo__bindgen_ty_bindgen_id_4__bindgen_ty_bindgen_id_7 {
+    fn clone(&self) -> Self { *self }
+}
+#[repr(C)]
+#[derive(Debug, Copy)]
+pub struct foo__bindgen_ty_bindgen_id_4__bindgen_ty_bindgen_id_12 {
+    pub d1: ::std::os::raw::c_uchar,
+    pub d2: ::std::os::raw::c_uchar,
+    pub d3: ::std::os::raw::c_uchar,
+    pub d4: ::std::os::raw::c_uchar,
+}
+#[test]
+fn bindgen_test_layout_foo__bindgen_ty_bindgen_id_4__bindgen_ty_bindgen_id_12() {
+    assert_eq!(::std::mem::size_of::<foo__bindgen_ty_bindgen_id_4__bindgen_ty_bindgen_id_12>()
+               , 4usize);
+    assert_eq!(::std::mem::align_of::<foo__bindgen_ty_bindgen_id_4__bindgen_ty_bindgen_id_12>()
+               , 1usize);
+}
+impl Clone for foo__bindgen_ty_bindgen_id_4__bindgen_ty_bindgen_id_12 {
     fn clone(&self) -> Self { *self }
 }
 #[test]
--- tests/expectations/union_with_nesting.rs
+++ tests/expectations/union_with_nesting.rs
@@ -35,7 +35,7 @@ pub struct foo {
 #[derive(Debug, Copy)]
 pub struct foo__bindgen_ty_bindgen_id_4 {
     pub __bindgen_anon_1: foo__bindgen_ty_bindgen_id_4__bindgen_ty_bindgen_id_5,
-    pub __bindgen_anon_2: foo__bindgen_ty_bindgen_id_4__bindgen_ty_bindgen_id_5,
+    pub __bindgen_anon_2: foo__bindgen_ty_bindgen_id_4__bindgen_ty_bindgen_id_10,
 }
 #[repr(C)]
 #[derive(Debug, Copy)]
@@ -52,6 +52,23 @@ fn bindgen_test_layout_foo__bindgen_ty_bindgen_id_4__bindgen_ty_bindgen_id_5() {
                , 2usize);
 }
 impl Clone for foo__bindgen_ty_bindgen_id_4__bindgen_ty_bindgen_id_5 {
+    fn clone(&self) -> Self { *self }
+}
+#[repr(C)]
+#[derive(Debug, Copy)]
+pub struct foo__bindgen_ty_bindgen_id_4__bindgen_ty_bindgen_id_10 {
+    pub c1: __BindgenUnionField<::std::os::raw::c_ushort>,
+    pub c2: __BindgenUnionField<::std::os::raw::c_ushort>,
+    pub bindgen_union_field: u16,
+}
+#[test]
+fn bindgen_test_layout_foo__bindgen_ty_bindgen_id_4__bindgen_ty_bindgen_id_10() {
+    assert_eq!(::std::mem::size_of::<foo__bindgen_ty_bindgen_id_4__bindgen_ty_bindgen_id_10>()
+               , 2usize);
+    assert_eq!(::std::mem::align_of::<foo__bindgen_ty_bindgen_id_4__bindgen_ty_bindgen_id_10>()
+               , 2usize);
+}
+impl Clone for foo__bindgen_ty_bindgen_id_4__bindgen_ty_bindgen_id_10 {
     fn clone(&self) -> Self { *self }
 }
 #[test]
