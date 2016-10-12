5912f088eeacff368f22aa1242a5ccc57628b6ac
Nick Fitzgerald <fitzgen@gmail.com>
2016-10-12 15:07:26 -0700

    Turn the two noisiest error!s into warn!s

--- src/ir/ty.rs
+++ src/ir/ty.rs
@@ -468,7 +468,7 @@ impl Type {
                                 return Err(ParseError::Recurse);
                             }
 
-                            error!("invalid type {:?}", ty);
+                            warn!("invalid type {:?}", ty);
                             return Err(ParseError::Continue);
                         }
                     }
