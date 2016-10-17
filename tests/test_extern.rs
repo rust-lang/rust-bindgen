9426353cba332098c1343c0388e8a922f46e43bd
Nick Fitzgerald <fitzgen@gmail.com>
2016-10-17 13:10:47 -0700

    Remove old tests that no longer build and run

--- tests/test_extern.rs
+++
@@ -1,8 +0,0 @@
-use support::assert_bind_eq;
-
-#[test]
-fn extern_c_in_hpp() {
-    assert_bind_eq(Default::default(), "headers/extern.hpp", "
-        pub type foo = unsafe extern \"C\" fn(bar: ::std::os::raw::c_int) -> ::std::os::raw::c_int;
-    ");
-}
