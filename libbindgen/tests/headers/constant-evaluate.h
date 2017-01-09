a20cf85dbb8b9f4ffcd656b3a541de24f4c37d95
Emilio Cobos Álvarez <emilio@crisal.io>
2017-01-09 23:05:20 +0100

    ir: Evaluate constant strings too.
    Signed-off-by: Emilio Cobos Álvarez <emilio@crisal.io>

--- libbindgen/tests/headers/constant-evaluate.h
+++ libbindgen/tests/headers/constant-evaluate.h
@@ -14,3 +14,6 @@ const long long BAZ = (1 << foo) | bar;
 const double fuzz = (1 + 50.0f);
 const char BAZZ = '5';
 const char WAT = '\0';
+
+const char* bytestring = "Foo";
+const char* NOT_UTF8 = "\xf0\x28\x8c\x28";
