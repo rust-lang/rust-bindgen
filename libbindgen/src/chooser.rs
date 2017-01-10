be1c760928cdcf32e96bc9ff2dacddd4548be40d
Emilio Cobos Álvarez <emilio@crisal.io>
2017-01-10 12:31:02 +0100

    chooser: Add a programmatic API for choosing whether to constify an enum variant.

--- libbindgen/src/chooser.rs
+++ libbindgen/src/chooser.rs
@@ -1,6 +1,7 @@
 //! A public API for more fine-grained customization of bindgen behavior.
 
 pub use ir::int::IntKind;
+pub use ir::enum_ty::EnumVariantValue;
 use std::fmt;
 
 /// A trait to allow configuring different kinds of types in different
@@ -10,5 +11,15 @@ pub trait TypeChooser: fmt::Debug {
     /// value of that macro, or `None` if you want the default to be chosen.
     fn int_macro(&self, _name: &str, _value: i64) -> Option<IntKind> {
         None
+    }
+
+    /// This function should return whether, given the a given enum variant
+    /// name, and value, returns whether this enum variant will forcibly be a
+    /// constant.
+    fn constify_enum_variant(&self,
+                             _enum_name: Option<&str>,
+                             _variant_name: &str,
+                             _variant_value: Option<EnumVariantValue>) -> bool {
+        false
     }
 }
