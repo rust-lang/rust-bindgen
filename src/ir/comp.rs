eba4f310541141e2c5cfd749976f80574c4f6b8e
Nick Fitzgerald <fitzgen@gmail.com>
2017-01-20 14:00:43 -0800

    Trace methods in CompInfo's TypeCollector impl
    Fixes #410

--- src/ir/comp.rs
+++ src/ir/comp.rs
@@ -957,6 +957,10 @@ impl TypeCollector for CompInfo {
             types.insert(var);
         }
 
-        // FIXME(emilio): Methods, VTable?
+        for method in self.methods() {
+            types.insert(method.signature);
+        }
+
+        // FIXME(emilio): VTable?
     }
 }
