86af08fb32ecbab92c50ebb0c7a701ada54aa0bf
Emilio Cobos Álvarez <emilio@crisal.io>
2017-01-31 23:45:48 +0100

    Re-enable OSX builds on Travis CI
    This reverts commit fc38f9b18a9dacc76b7e8a5c8633f0f02289daae.

--- ci/before_install.sh
+++ ci/before_install.sh
@@ -1,6 +1,11 @@
 set -e
 pushd ~
 
+# Workaround for Travis CI macOS bug (https://github.com/travis-ci/travis-ci/issues/6307)
+if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
+    rvm get head || true
+fi
+
 function llvm_version_triple() {
     if [ "$1" == "3.8" ]; then
         echo "3.8.0"
@@ -18,9 +23,17 @@ function llvm_download() {
     tar -xf ${LLVM}.tar.xz -C llvm --strip-components=1
 
     export LLVM_CONFIG_PATH=`pwd`/llvm/bin/llvm-config
+    if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
+        cp llvm/lib/libclang.dylib /usr/local/lib/libclang.dylib
+    fi
 }
 
-llvm_download linux-gnu-ubuntu-14.04
+
+if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
+    llvm_download linux-gnu-ubuntu-14.04
+else
+    llvm_download apple-darwin
+fi
 
 popd
 set +e
