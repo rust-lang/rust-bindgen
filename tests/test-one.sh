a909b96c3e258ccd20535e8a16e6c88e058b25ef
Nick Fitzgerald <fitzgen@gmail.com>
2017-05-17 09:38:38 -0700

    Small improvements to test-one.sh
    Allow running tests that don't have `bindgen-flags` pragmas. Less noisy
    output. Show diff if the generated bindings differ from the expected bindings.

--- tests/test-one.sh
+++ tests/test-one.sh
@@ -8,7 +8,7 @@
 # `rustc` to compile the bindings with unit tests enabled, and run the generated
 # layout tests.
 
-set -eux
+set -eu
 
 cd $(dirname $0)
 cd ..
@@ -21,7 +21,7 @@ TEST=$(find ./tests/headers -type f -iname "*$1*" | head -n 1)
 BINDINGS=$(mktemp -t bindings.rs.XXXXXX)
 TEST_BINDINGS_BINARY=$(mktemp -t bindings.XXXXXX)
 
-FLAGS="$(grep "// bindgen-flags: " "$TEST")"
+FLAGS="$(grep "// bindgen-flags: " "$TEST" || echo)"
 FLAGS="${FLAGS/\/\/ bindgen\-flags:/}"
 
 eval ./target/debug/bindgen \
@@ -34,14 +34,36 @@ eval ./target/debug/bindgen \
 
 dot -Tpng ir.dot -o ir.png
 
+echo
 echo "=== Input header ========================================================"
+echo
+
 cat "$TEST"
 
+echo
 echo "=== Generated bindings =================================================="
+echo
+
 cat "$BINDINGS"
 
+echo
+echo "=== Diff w/ expected bindings ==========================================="
+echo
+
+EXPECTED=${TEST/headers/expectations\/tests}
+EXPECTED=${EXPECTED/.hpp/.rs}
+
+# Don't exit early if there is a diff.
+diff -U8 "$EXPECTED" "$BINDINGS" || true
+
+echo
 echo "=== Building bindings ==================================================="
+echo
+
 rustc --test -o "$TEST_BINDINGS_BINARY" "$BINDINGS" --crate-name bindgen_test_one
 
+echo
 echo "=== Testing bindings ===================================================="
+echo
+
 "$TEST_BINDINGS_BINARY"
