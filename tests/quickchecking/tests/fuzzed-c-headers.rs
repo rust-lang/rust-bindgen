b4ba0923fb00c916584fd2f79455bf179efa1558
Shea Newton <sheanewt@gmail.com>
2017-12-03 18:29:06 -0800

    Quickchecking crate CLI
    Prior to this commit the quickchecking crate used for generating proprty
    tests
    for bindgen was a [lib] target and had configurations that required
    commenting/uncommenting code to enable/disable. This meant it was
    inconvienent/prohibitive to configure the property tests on a per-run
    basis.
    This commit reorganizes the `quickchecking` crate to provide both [lib]
    and
    [[bin]] targets in order to expose those configurations through a CLI.
    
    The configurations that are exposed through the [[bin]] target's CLI
    are:
    * Count/number of tests to run.
    * Directory to provide fuzzed headers
    * Generation range corresponding to the range quickcheck uses to
    * generate arbitrary.
    
    __Usage from the__ `tests/quickchecking` __directory__
    ```bash
    quickchecking 0.2.0
    Bindgen property tests with quickcheck. Generate random valid C code and
    pass it to the csmith/predicate.py script
    
    USAGE:
        quickchecking [OPTIONS]
    
    FLAGS:
        -h, --help       Prints help information
        -V, --version    Prints version information
    
    OPTIONS:
        -c, --count <COUNT>    Count / number of tests to run. Running a
    fuzzed header through the predicate.py script can
                               take a long time, especially if the
    generation range is large. Increase this number if you're
                               willing to wait a while. [default: 2]
        -p, --path <PATH>      Optional. Preserve generated headers for
    inspection, provide directory path for header
                               output. [default: None]
        -r, --range <RANGE>    Sets the range quickcheck uses during
    generation. Corresponds to things like arbitrary usize
                               and arbitrary vector length. This number
    doesn't have to grow much for that execution time to
                               increase significantly. [default: 32]
    ```
    
    Because the actual work of running the property tests moved to the
    [[bin]]
    target, rather than duplicate that code in the `quickchecking` crate's
    tests
    directory, some actual (very basic) tests for the `quickchecking` crate
    were
    added.
    
    *Note: I'm not attached to any of the option flags, if there are better
    characters/words for any of the options I've exposed I'll be happy to
    revise!
    
    Thanks for taking a look, looking forward to feedback!
    
    Closes #1168
    
    r? @fitzgen

--- tests/quickchecking/tests/fuzzed-c-headers.rs
+++ tests/quickchecking/tests/fuzzed-c-headers.rs
@@ -1,78 +1,95 @@
+
 extern crate quickcheck;
 extern crate quickchecking;
 extern crate rand;
-extern crate tempdir;
 
-use quickchecking::fuzzers;
-use quickcheck::{QuickCheck, StdGen, TestResult};
-use std::fs::File;
-use std::io::Write;
-use tempdir::TempDir;
-use std::process::{Command, Output};
-use std::path::PathBuf;
-use std::error::Error;
+use quickchecking::fuzzers::{ArrayDimensionC, BaseTypeC, BasicTypeDeclarationC, DeclarationC,
+                             DeclarationListC, FunctionPointerDeclarationC, FunctionPrototypeC,
+                             HeaderC, ParameterC, ParameterListC, PointerLevelC,
+                             StructDeclarationC, TypeQualifierC, UnionDeclarationC};
+use quickcheck::{Arbitrary, StdGen};
 use rand::thread_rng;
 
-fn run_predicate_script(header: fuzzers::HeaderC, header_name: &str) -> Result<Output, Box<Error>> {
-    let dir = TempDir::new("bindgen_prop")?;
-    let header_path = dir.path().join(header_name);
-
-    let mut header_file = File::create(&header_path)?;
-    header_file.write_all(header.to_string().as_bytes())?;
-    header_file.sync_all()?;
-
-    let header_path_string;
-    match header_path.into_os_string().into_string() {
-        Ok(s) => header_path_string = s,
-        Err(_) => return Err(From::from("error converting path into String")),
-    }
-
-    let mut predicate_script_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    predicate_script_path.push("../../csmith-fuzzing/predicate.py");
-
-    let predicate_script_path_string;
-    match predicate_script_path.into_os_string().into_string() {
-        Ok(s) => predicate_script_path_string = s,
-        Err(_) => return Err(From::from("error converting path into String")),
-    }
-
-    // Copy generated temp files to test directory for inspection.
-    // Preserved for anyone interested in validating the behavior.
-
-    let mut debug_output_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    debug_output_path.push("tests");
-    Command::new("cp")
-        .arg("-a")
-        .arg(&dir.path().to_str().unwrap())
-        .arg(&debug_output_path.to_str().unwrap())
-        .output()?;
-
-    Ok(Command::new(&predicate_script_path_string)
-        .arg(&header_path_string)
-        .output()?)
-}
-
-fn bindgen_prop(header: fuzzers::HeaderC) -> TestResult {
-    match run_predicate_script(header, "prop_test.h") {
-        Ok(o) => return TestResult::from_bool(o.status.success()),
-        Err(e) => {
-            println!("{:?}", e);
-            return TestResult::from_bool(false);
-        }
-    }
+#[test]
+fn test_declaraion_c_does_not_panic() {
+    let ref mut gen = StdGen::new(thread_rng(), 50);
+    let _: DeclarationC = Arbitrary::arbitrary(gen);
 }
 
 #[test]
-fn test_bindgen() {
-    // Enough to generate any value in the PrimitiveTypeC `base_type` list.
-    let generate_range: usize = 32;
-    QuickCheck::new()
-        // Generating is relatively quick (generate_range 150 takes ~5 seconds)
-        // but running predicate.py takes ~30 seconds per source file / test
-        // when the generation range is just 32. It can take a lot longer with a
-        // higher generate_range. Up the number of tests or generate_range if
-        // you're willing to wait awhile.
-        .tests(2)
-        .gen(StdGen::new(thread_rng(), generate_range))
-        .quickcheck(bindgen_prop as fn(fuzzers::HeaderC) -> TestResult)
+fn test_declaraion_list_c_does_not_panic() {
+    let ref mut gen = StdGen::new(thread_rng(), 50);
+    let _: DeclarationListC = Arbitrary::arbitrary(gen);
+}
+
+#[test]
+fn test_base_type_c_does_not_panic() {
+    let ref mut gen = StdGen::new(thread_rng(), 50);
+    let _: BaseTypeC = Arbitrary::arbitrary(gen);
+}
+
+#[test]
+fn test_type_qualifier_c_does_not_panic() {
+    let ref mut gen = StdGen::new(thread_rng(), 50);
+    let _: TypeQualifierC = Arbitrary::arbitrary(gen);
+}
+
+#[test]
+fn test_pointer_level_c_does_not_panic() {
+    let ref mut gen = StdGen::new(thread_rng(), 50);
+    let _: PointerLevelC = Arbitrary::arbitrary(gen);
+}
+
+#[test]
+fn test_array_dimension_c_does_not_panic() {
+    let ref mut gen = StdGen::new(thread_rng(), 50);
+    let _: ArrayDimensionC = Arbitrary::arbitrary(gen);
+}
+
+#[test]
+fn test_basic_type_declaration_c_does_not_panic() {
+    let ref mut gen = StdGen::new(thread_rng(), 50);
+    let _: BasicTypeDeclarationC = Arbitrary::arbitrary(gen);
+}
+
+#[test]
+fn test_struct_declaration_c_does_not_panic() {
+    let ref mut gen = StdGen::new(thread_rng(), 50);
+    let _: StructDeclarationC = Arbitrary::arbitrary(gen);
+}
+
+#[test]
+fn test_union_declaration_c_does_not_panic() {
+    let ref mut gen = StdGen::new(thread_rng(), 50);
+    let _: UnionDeclarationC = Arbitrary::arbitrary(gen);
+}
+
+#[test]
+fn test_function_pointer_declaration_c_does_not_panic() {
+    let ref mut gen = StdGen::new(thread_rng(), 50);
+    let _: FunctionPointerDeclarationC = Arbitrary::arbitrary(gen);
+}
+
+#[test]
+fn test_function_prototype_c_does_not_panic() {
+    let ref mut gen = StdGen::new(thread_rng(), 50);
+    let _: FunctionPrototypeC = Arbitrary::arbitrary(gen);
+}
+
+#[test]
+fn test_parameter_c_does_not_panic() {
+    let ref mut gen = StdGen::new(thread_rng(), 50);
+    let _: ParameterC = Arbitrary::arbitrary(gen);
+}
+
+#[test]
+fn test_parameter_list_c_does_not_panic() {
+    let ref mut gen = StdGen::new(thread_rng(), 50);
+    let _: ParameterListC = Arbitrary::arbitrary(gen);
+}
+
+#[test]
+fn test_header_c_does_not_panic() {
+    let ref mut gen = StdGen::new(thread_rng(), 50);
+    let _: HeaderC = Arbitrary::arbitrary(gen);
 }
