cb12a31c885d6b32011f0295c9144e021296ee61
Emilio Cobos Álvarez <emilio@crisal.io>
2018-09-20 15:45:08 +0200

    codegen: Explicitly align unions if needed.
    Also adds a test header I missed from the previous PR.
    
    Fixes #1393

---
+++ tests/headers/long_double.h
@@ -0,0 +1,5 @@
+// bindgen-flags: --rust-target 1.26
+
+struct foo {
+  long double bar;
+};
---
+++ tests/headers/union-align.h
@@ -0,0 +1,5 @@
+// bindgen-flags: --rust-target 1.26
+
+union Bar {
+  unsigned char foo;
+} __attribute__ ((__aligned__ (16)));
