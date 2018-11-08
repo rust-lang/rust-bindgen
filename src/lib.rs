0275c4cb30aa4b30016ff79c5e092955a80ec238
Josh Channings <josh@channings.me.uk>
2018-11-08 14:29:35 +0000

    Fix Builder::command_line_args() --generate typo
    Looks like the accepted argument has always been "functions", and this output has always been "function".

--- src/lib.rs
+++ src/lib.rs
@@ -431,7 +431,7 @@ impl Builder {
         //Temporary placeholder for below 4 options
         let mut options: Vec<String> = Vec::new();
         if self.options.codegen_config.functions() {
-            options.push("function".into());
+            options.push("functions".into());
         }
         if self.options.codegen_config.types() {
             options.push("types".into());
