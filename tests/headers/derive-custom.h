94698e02a6c346ed20a2bf6d4217be9e308ddb15
Porter Smith <flowbish@gmail.com>
2018-11-28 14:45:08 -0800

    Add source annotation to express explicit derives for a type.
    This allows for explicit selection of which traits are derived for a
    type, e.g. deriving custom traits on particular types that need it.
    
    Example usage:
    
    ```C++
    /// <div rustbindgen derive="Clone"></div>
    /// <div rustbindgen derive="MyDerivableTrait"></div>
    struct foo { ...  };
    ```
    
    generates into
    
    ```Rust
     #[derive(Clone,MyDerivableTrait)]
     struct foo { ... }
    ```

---
+++ tests/headers/derive-custom.h
@@ -0,0 +1,28 @@
+// bindgen-flags: --no-derive-debug --no-derive-copy --default-enum-style rust
+
+/** <div rustbindgen derive="Debug"></div> */
+struct my_type;
+
+/** <div rustbindgen derive="Clone"></div> */
+struct my_type;
+
+struct my_type {
+    int a;
+};
+
+/**
+ * <div rustbindgen derive="Debug"></div>
+ * <div rustbindgen derive="Clone"></div>
+ */
+struct my_type2;
+
+struct my_type2 {
+    unsigned a;
+};
+
+/**
+ * <div rustbindgen derive="Debug" derive="Clone"></div>
+ */
+struct my_type3 {
+    unsigned long a;
+};
