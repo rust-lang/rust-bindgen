7ba2cae8c825baf411488ded7d0eb1e6cc17452d
uk <50893351+unterkontrolle@users.noreply.github.com>
2019-06-12 17:23:19 -0300

    Add missing --rust-target flags on test case.
    See https://github.com/rust-lang/rust-bindgen/pull/1575#discussion_r293079226

--- tests/headers/issue-1554.h
+++ tests/headers/issue-1554.h
@@ -1,4 +1,4 @@
-// bindgen-flags: --default-enum-style rust_non_exhaustive --raw-line '#![cfg(feature = "nightly")]' --raw-line '#![feature(non_exhaustive)]'
+// bindgen-flags: --default-enum-style rust_non_exhaustive --rust-target nightly --raw-line '#![cfg(feature = "nightly")]' --raw-line '#![feature(non_exhaustive)]'
 
 enum Planet {
     earth,
