d0d0726615e1891a66282e77d2b1daba072570cf
Nico Chatzi <nico.chatzigianis@focusrite.com>
2021-04-10 17:26:07 +0100

    Fix docstring comment for constants

---
+++ tests/headers/issue-1995.h
@@ -0,0 +1,12 @@
+/// This is a constant that has a docstring
+///
+/// And expected to be found in generated bindings code too.
+const int FOO = 1;
+
+/// This is a constant that has a docstring
+///
+/// And expected to be found in generated bindings code too.
+struct Bar
+{
+    int baz;
+};
