57e5c2c3772e64fe48f70d46b2ad2e85a08cb07c
Adrian Taylor <adetaylor@chromium.org>
2021-04-23 14:37:13 -0700

    Remove a test case.
    When dependent qualified types are supported, this runs into
    a different bug, #1913, where we generate an infinitely deep
    structure. The reduced template here doesn't really make any
    sense and wouldn't be encountered in the wild quite like this.
    
    Previously we generated opaque types for this case, so we didn't
    run into that bug. We now generate higher-fidelity types and therefore
    hit the problem.
    
    I can't see any way to salvage the fundamentals of this test case
    without the recursive problem, so I propose to remove it.

--- tests/headers/issue-544-stylo-creduce-2.hpp
+++
@@ -1,8 +0,0 @@
-// bindgen-flags: -- -std=c++14
-
-template <typename T>
-struct Foo {
-    template <typename> using FirstAlias = typename T::Associated;
-    template <typename U> using SecondAlias = Foo<FirstAlias<U>>;
-    SecondAlias<int> member;
-};
