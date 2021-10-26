e73e19091331bce9099ec7e3a199309b6a3db35a
Mikuroさいな <ryosukadnak@gmail.com>
2021-10-26 12:58:54 +0900

    Fix log args

--- src/log_stubs.rs
+++ src/log_stubs.rs
@@ -11,22 +11,22 @@ macro_rules! log {
     }
 }
 macro_rules! error {
-    (target: $target:expr, $($arg:tt)*) => { log!($target, $($arg)*); }
+    (target: $target:expr, $($arg:tt)*) => { log!(target: $target, "", $($arg)*); }
     ($($arg:tt)*) => { log!("", $($arg)*); }
 }
 macro_rules! warn {
-    (target: $target:expr, $($arg:tt)*) => { log!($target, $($arg)*); }
+    (target: $target:expr, $($arg:tt)*) => { log!(target: $target, "", $($arg)*); }
     ($($arg:tt)*) => { log!("", $($arg)*); }
 }
 macro_rules! info {
-    (target: $target:expr, $($arg:tt)*) => { log!($target, $($arg)*); }
+    (target: $target:expr, $($arg:tt)*) => { log!(target: $target, "", $($arg)*); }
     ($($arg:tt)*) => { log!("", $($arg)*); }
 }
 macro_rules! debug {
-    (target: $target:expr, $($arg:tt)*) => { log!($target, $($arg)*); }
+    (target: $target:expr, $($arg:tt)*) => { log!(target: $target, "", $($arg)*); }
     ($($arg:tt)*) => { log!("", $($arg)*); }
 }
 macro_rules! trace {
-    (target: $target:expr, $($arg:tt)*) => { log!($target, $($arg)*); }
+    (target: $target:expr, $($arg:tt)*) => { log!(target: $target, "", $($arg)*); }
     ($($arg:tt)*) => { log!("", $($arg)*); }
 }
