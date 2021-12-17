cf3658b6ad303fab28fd7d902de08b3fab05087c
Emilio Cobos Álvarez <emilio@crisal.io>
2021-12-18 00:48:19 +0100

    options: Implement --version manually and print clang version on --version --verbose
    Fixes #2138

--- src/options.rs
+++ src/options.rs
@@ -23,13 +23,13 @@ where
     );
 
     let matches = App::new("bindgen")
-        .version(option_env!("CARGO_PKG_VERSION").unwrap_or("unknown"))
         .about("Generates Rust bindings from C/C++ headers.")
+        .setting(clap::AppSettings::NoAutoVersion)
         .override_usage("bindgen [FLAGS] [OPTIONS] <header> -- <clang-args>...")
         .args(&[
             Arg::new("header")
                 .help("C or C++ header file")
-                .required(true),
+                .required_unless_present("V"),
             Arg::new("depfile")
                 .long("depfile")
                 .takes_value(true)
@@ -545,8 +545,23 @@ where
             Arg::new("vtable-generation")
                 .long("vtable-generation")
                 .help("Enables generation of vtable functions."),
+            Arg::new("V")
+                .long("version")
+                .help("Prints the version, and exits"),
         ]) // .args()
         .get_matches_from(args);
+
+    let verbose = matches.is_present("verbose");
+    if matches.is_present("V") {
+        println!(
+            "bindgen {}",
+            option_env!("CARGO_PKG_VERSION").unwrap_or("unknown")
+        );
+        if verbose {
+            println!("Clang: {}", crate::clang_version().full);
+        }
+        std::process::exit(0);
+    }
 
     let mut builder = builder();
 
@@ -1014,8 +1029,6 @@ where
     if matches.is_present("vtable-generation") {
         builder = builder.vtable_generation(true);
     }
-
-    let verbose = matches.is_present("verbose");
 
     Ok((builder, output, verbose))
 }
