2195fa46434329948d0395d39fc766c434976304
Emil Gardström <emil.gardstrom@gmail.com>
2022-02-25 21:39:34 +0100

    disable layout tests for `derive-custom` and `convert-cpp-comment-to-rust` test

--- tests/expectations/tests/convert-cpp-comment-to-rust.rs
+++ tests/expectations/tests/convert-cpp-comment-to-rust.rs
@@ -17,55 +17,6 @@ pub struct mbedtls_mpi {
     ///<  pointer to limbs
     pub p: *mut mbedtls_mpi_uint,
 }
-#[test]
-fn bindgen_test_layout_mbedtls_mpi() {
-    assert_eq!(
-        ::std::mem::size_of::<mbedtls_mpi>(),
-        24usize,
-        concat!("Size of: ", stringify!(mbedtls_mpi))
-    );
-    assert_eq!(
-        ::std::mem::align_of::<mbedtls_mpi>(),
-        8usize,
-        concat!("Alignment of ", stringify!(mbedtls_mpi))
-    );
-    assert_eq!(
-        unsafe {
-            &(*(::std::ptr::null::<mbedtls_mpi>())).s as *const _ as usize
-        },
-        0usize,
-        concat!(
-            "Offset of field: ",
-            stringify!(mbedtls_mpi),
-            "::",
-            stringify!(s)
-        )
-    );
-    assert_eq!(
-        unsafe {
-            &(*(::std::ptr::null::<mbedtls_mpi>())).n as *const _ as usize
-        },
-        8usize,
-        concat!(
-            "Offset of field: ",
-            stringify!(mbedtls_mpi),
-            "::",
-            stringify!(n)
-        )
-    );
-    assert_eq!(
-        unsafe {
-            &(*(::std::ptr::null::<mbedtls_mpi>())).p as *const _ as usize
-        },
-        16usize,
-        concat!(
-            "Offset of field: ",
-            stringify!(mbedtls_mpi),
-            "::",
-            stringify!(p)
-        )
-    );
-}
 impl Default for mbedtls_mpi {
     fn default() -> Self {
         let mut s = ::std::mem::MaybeUninit::<Self>::uninit();
