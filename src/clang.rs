fb8b89a0aa93ed6f8573dd9c230286e6b0a99e6c
Darren Kulp <darren@kulp.ch>
2022-06-23 20:48:44 -0400

    Implement enum_val_boolean in terms of unsigned

--- src/clang.rs
+++ src/clang.rs
@@ -517,13 +517,7 @@ impl Cursor {
     ///
     /// Returns None if the cursor's referent is not an enum variant.
     pub fn enum_val_boolean(&self) -> Option<bool> {
-        unsafe {
-            if self.kind() == CXCursor_EnumConstantDecl {
-                Some(clang_getEnumConstantDeclValue(self.x) != 0)
-            } else {
-                None
-            }
-        }
+        self.enum_val_unsigned().map(|x| x != 0)
     }
 
     /// Get the signed constant value for this cursor's enum variant referent.
