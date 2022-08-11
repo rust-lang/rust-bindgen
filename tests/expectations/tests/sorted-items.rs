29740bb7410662da7a1c9b73f04c65a614acc4ab
Amanjeev Sethi <aj@amanjeev.com>
2022-08-11 16:42:49 -0400

    feat(lib sorting): test fix

--- tests/expectations/tests/sorted-items.rs
+++ tests/expectations/tests/sorted-items.rs
@@ -8,14 +8,21 @@
 extern "C" {
     pub fn foo() -> ::std::os::raw::c_int;
 }
+pub type number = ::std::os::raw::c_int;
 extern "C" {
     pub fn bar(x: number) -> ::std::os::raw::c_int;
 }
-extern "C" {
-    pub fn baz(point: Point) -> ::std::os::raw::c_int;
+#[repr(C)]
+#[derive(Debug, Default, Copy, Clone)]
+pub struct Point {
+    pub x: number,
+    pub y: number,
 }
 #[test]
 fn bindgen_test_layout_Point() {
+    const UNINIT: ::std::mem::MaybeUninit<Point> =
+        ::std::mem::MaybeUninit::uninit();
+    let ptr = UNINIT.as_ptr();
     assert_eq!(
         ::std::mem::size_of::<Point>(),
         8usize,
@@ -27,21 +34,17 @@ fn bindgen_test_layout_Point() {
         concat!("Alignment of ", stringify!(Point))
     );
     assert_eq!(
-        unsafe { &(*(::std::ptr::null::<Point>())).x as *const _ as usize },
+        unsafe { ::std::ptr::addr_of!((*ptr).x) as usize - ptr as usize },
         0usize,
         concat!("Offset of field: ", stringify!(Point), "::", stringify!(x))
     );
     assert_eq!(
-        unsafe { &(*(::std::ptr::null::<Point>())).y as *const _ as usize },
+        unsafe { ::std::ptr::addr_of!((*ptr).y) as usize - ptr as usize },
         4usize,
         concat!("Offset of field: ", stringify!(Point), "::", stringify!(y))
     );
 }
-pub const NUMBER: number = 42;
-#[repr(C)]
-#[derive(Debug, Copy, Clone)]
-pub struct Point {
-    pub x: number,
-    pub y: number,
+extern "C" {
+    pub fn baz(point: Point) -> ::std::os::raw::c_int;
 }
-pub type number = ::std::os::raw::c_int;
+pub const NUMBER: number = 42;
