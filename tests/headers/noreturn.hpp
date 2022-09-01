86f059f081160ba01de31fc2ec6e20e52b97968d
Christian Poveda <christian.poveda@ferrous-systems.com>
2022-09-01 16:24:38 -0500

    add the `-- -std=c++11` flag

--- tests/headers/noreturn.hpp
+++ tests/headers/noreturn.hpp
@@ -1,5 +1,4 @@
-// bindgen-flags: --enable-function-attribute-detection
+// bindgen-flags: --enable-function-attribute-detection -- -std=c++11
 _Noreturn void f(void);
-// TODO (pvdrz): figure out how to handle this case.
 __attribute__((noreturn)) void g(void);
 [[noreturn]] void h(void);
