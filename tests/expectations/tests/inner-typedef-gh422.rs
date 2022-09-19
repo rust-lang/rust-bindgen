362341d53448196369a9e2a6242a26c305527c6a
Amanjeev Sethi <aj@amanjeev.com>
2022-09-19 19:26:44 -0400

    test: add test for GH-422
    GitHub issue 422 was fixed but needs a test.
    https://github.com/rust-lang/rust-bindgen/issues/422
    
    Signed-off-by: Amanjeev Sethi <aj@amanjeev.com>

---
+++ tests/expectations/tests/inner-typedef-gh422.rs
@@ -0,0 +1,37 @@
+#![allow(
+    dead_code,
+    non_snake_case,
+    non_camel_case_types,
+    non_upper_case_globals
+)]
+
+#[repr(C)]
+#[derive(Debug, Default, Copy, Clone)]
+pub struct Foo {
+    pub _address: u8,
+}
+#[repr(C)]
+#[derive(Debug, Copy, Clone)]
+pub struct Foo_InnerType<T> {
+    pub t: T,
+    pub _phantom_0: ::std::marker::PhantomData<::std::cell::UnsafeCell<T>>,
+}
+impl<T> Default for Foo_InnerType<T> {
+    fn default() -> Self {
+        let mut s = ::std::mem::MaybeUninit::<Self>::uninit();
+        unsafe {
+            ::std::ptr::write_bytes(s.as_mut_ptr(), 0, 1);
+            s.assume_init()
+        }
+    }
+}
+pub type Bar = InnerType;
+extern "C" {
+    #[link_name = "\u{1}_Z4funcv"]
+    pub fn func() -> Bar;
+}
+#[repr(C)]
+#[derive(Debug, Default, Copy, Clone)]
+pub struct InnerType {
+    pub _address: u8,
+}
