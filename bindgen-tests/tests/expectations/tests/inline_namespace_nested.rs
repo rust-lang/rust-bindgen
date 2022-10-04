c3db283350cbb18a325a9c4b0ad6745f1f1c4e35
Emilio Cobos Álvarez <emilio@crisal.io>
2022-10-04 16:54:03 +0200

    context: Fix tokenization of C++20 inline namespace.
    Fixes #2293

---
+++ bindgen-tests/tests/expectations/tests/inline_namespace_nested.rs
@@ -0,0 +1,21 @@
+#![allow(
+    dead_code,
+    non_snake_case,
+    non_camel_case_types,
+    non_upper_case_globals
+)]
+
+#[allow(non_snake_case, non_camel_case_types, non_upper_case_globals)]
+pub mod root {
+    #[allow(unused_imports)]
+    use self::super::root;
+    pub mod ranges {
+        #[allow(unused_imports)]
+        use self::super::super::root;
+        pub mod bar {
+            #[allow(unused_imports)]
+            use self::super::super::super::root;
+            pub const bar: ::std::os::raw::c_int = 0;
+        }
+    }
+}
