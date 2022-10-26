fad1b508934a25ff9351f92b98f53a6c848b13d6
David Tolnay <dtolnay@gmail.com>
2022-10-26 15:07:07 -0700

    Add test of enum and typedef with same name
        error[E0428]: the name `Enum` is defined multiple times
          --> bindgen-tests/tests/expectations/tests/enum-typedef.rs:10:1
           |
        9  | pub type Enum = ::std::os::raw::c_uint;
           | --------------------------------------- previous definition of the type `Enum` here
        10 | pub type Enum = i16;
           | ^^^^^^^^^^^^^^^^^^^^ `Enum` redefined here
           |
           = note: `Enum` must be defined only once in the type namespace of this module
    
        error[E0428]: the name `TypedefFirst` is defined multiple times
          --> bindgen-tests/tests/expectations/tests/enum-typedef.rs:13:1
           |
        11 | pub type TypedefFirst = i16;
           | ---------------------------- previous definition of the type `TypedefFirst` here
        12 | pub const TypedefFirst_Variant2: TypedefFirst = 0;
        13 | pub type TypedefFirst = ::std::os::raw::c_uint;
           | ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ `TypedefFirst` redefined here
           |
           = note: `TypedefFirst` must be defined only once in the type namespace of this module

---
+++ bindgen-tests/tests/expectations/tests/enum-typedef.rs
@@ -0,0 +1,13 @@
+#![allow(
+    dead_code,
+    non_snake_case,
+    non_camel_case_types,
+    non_upper_case_globals
+)]
+
+pub const Enum_Variant: Enum = 0;
+pub type Enum = ::std::os::raw::c_uint;
+pub type Enum = i16;
+pub type TypedefFirst = i16;
+pub const TypedefFirst_Variant2: TypedefFirst = 0;
+pub type TypedefFirst = ::std::os::raw::c_uint;
