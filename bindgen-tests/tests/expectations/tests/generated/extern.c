6721b968a94d204374c708486b485d3490ba9c6a
Christian Poveda <christian.poveda@ferrous-systems.com>
2023-02-02 13:17:05 -0500

    fix test

--- bindgen-tests/tests/expectations/tests/generated/extern.c
+++ bindgen-tests/tests/expectations/tests/generated/extern.c
@@ -3,4 +3,4 @@ int foo__extern() { return foo(); }
 int bar__extern() asm("bar__extern");
 int bar__extern() { return bar(); }
 int takes_ptr__extern(int* arg) asm("takes_ptr__extern");
-int takes_ptr__extern(int*arg) { return takes_ptr(arg); }
+int takes_ptr__extern(int* arg) { return takes_ptr(arg); }
