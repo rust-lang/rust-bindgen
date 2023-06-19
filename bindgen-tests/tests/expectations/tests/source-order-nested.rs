610c6f66884ce190a942c118167c2e27a2cd716e
Markus Reiter <me@reitermark.us>
2023-06-19 17:17:24 +0200

    Add source order test for headers that are siblings.

--- bindgen-tests/tests/expectations/tests/source-order-nested.rs
+++ bindgen-tests/tests/expectations/tests/source-order-nested.rs
@@ -7,19 +7,14 @@ pub type THIS_SHOULD_BE_THIRD = ::std::os::raw::c_int;
 #[repr(C)]
 #[derive(Debug, Default, Copy, Clone)]
 pub struct THIS_SHOULD_BE_FOURTH {}
-#[test]
-fn bindgen_test_layout_THIS_SHOULD_BE_FOURTH() {
-    assert_eq!(
-        ::std::mem::size_of::<THIS_SHOULD_BE_FOURTH>(),
-        0usize,
-        concat!("Size of: ", stringify!(THIS_SHOULD_BE_FOURTH)),
-    );
-    assert_eq!(
-        ::std::mem::align_of::<THIS_SHOULD_BE_FOURTH>(),
-        1usize,
-        concat!("Alignment of ", stringify!(THIS_SHOULD_BE_FOURTH)),
-    );
-}
+const _: () = {
+    [
+        "Size of THIS_SHOULD_BE_FOURTH",
+    ][::std::mem::size_of::<THIS_SHOULD_BE_FOURTH>() - 0usize];
+    [
+        "Alignment of THIS_SHOULD_BE_FOURTH",
+    ][::std::mem::align_of::<THIS_SHOULD_BE_FOURTH>() - 1usize];
+};
 extern "C" {
     pub static mut THIS_SHOULD_BE_FIFTH: ::std::os::raw::c_int;
 }
