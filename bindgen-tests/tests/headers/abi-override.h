1d2b579d1c3180e93089ffb1379ee11fac0cccc0
Tshepang Mbambo <tshepang.mbambo@ferrous-systems.com>
2023-07-19 20:57:20 +0200

    account for system abi (#2586)
    * account for system abi
    
    Closes #2576
    
    * add test for system abi
    
    * Update changelog
    
    ---------
    
    Co-authored-by: Christian Poveda <christian.poveda@ferrous-systems.com>

--- bindgen-tests/tests/headers/abi-override.h
+++ bindgen-tests/tests/headers/abi-override.h
@@ -1,8 +1,9 @@
-// bindgen-flags: --override-abi=foo=fastcall --override-abi=bar=stdcall --override-abi=boo=efiapi --override-abi=foobar=efiapi
+// bindgen-flags: --override-abi=foo=fastcall --override-abi=bar=stdcall --override-abi=boo=efiapi --override-abi=foobar=efiapi --override-abi qux=system
 
 void foo();
 void bar();
 void baz();
+void qux();
 
 typedef void (*boo)();
 typedef void (*foobar)(boo boo);
