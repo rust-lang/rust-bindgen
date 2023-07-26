38ab0b0c5a6f9add35e440935667de4d0f339175
Tshepang Mbambo <tshepang.mbambo@ferrous-systems.com>
2023-07-26 07:28:54 +0200

    adjust test to indicate it not longer requires nightly

--- bindgen-tests/tests/headers/c-unwind-abi-override-nightly.h
+++ bindgen-tests/tests/headers/c-unwind-abi-override-nightly.h
@@ -1,4 +1,4 @@
-// bindgen-flags: --override-abi="foo|bar=C-unwind" --rust-target=nightly --raw-line '#![cfg(feature = "nightly")]' --raw-line '#![feature(abi_thiscall)]'
+// bindgen-flags: --override-abi="foo|bar=C-unwind"
 
 void foo();
 void bar();
