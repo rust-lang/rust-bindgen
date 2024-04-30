877f3e0b9b184a99a3a4cad7ee0c65fd550913bd
Christian Poveda <git@pvdrz.com>
2024-04-30 11:12:01 -0500

    Add test

---
+++ bindgen-tests/tests/expectations/tests/anon_enum_allowlist_item.rs
@@ -0,0 +1,4 @@
+#![allow(dead_code, non_snake_case, non_camel_case_types, non_upper_case_globals)]
+pub const NODE_FLAG_FOO: _bindgen_ty_1 = 0;
+pub const NODE_FLAG_BAR: _bindgen_ty_1 = 1;
+pub type _bindgen_ty_1 = ::std::os::raw::c_uint;
---
+++ bindgen-tests/tests/headers/anon_enum_allowlist_item.h
@@ -0,0 +1,6 @@
+// bindgen-flags: --allowlist-item "NODE_.*"
+
+enum {
+  NODE_FLAG_FOO,
+  NODE_FLAG_BAR,
+};
