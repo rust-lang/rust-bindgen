22a1b72ea50242e67ccb0e8dbba60440a33671fd
Gabi Ganam <ggabi@amazon.com>
2024-06-25 07:19:18 +0000

    Fix generated constants: `f64::INFINITY`, `f64::NEG_ INFINITY`, `f64::NAN`
    https://github.com/rust-lang/rust-bindgen/issues/2853

--- bindgen-tests/tests/expectations/tests/infinite-macro.rs
+++ bindgen-tests/tests/expectations/tests/infinite-macro.rs
@@ -1,3 +1,3 @@
 #![allow(dead_code, non_snake_case, non_camel_case_types, non_upper_case_globals)]
-pub const INFINITY: f64 = ::std::f64::INFINITY;
-pub const NAN: f64 = ::std::f64::NAN;
+pub const INFINITY: f64 = f64::INFINITY;
+pub const NAN: f64 = f64::NAN;
