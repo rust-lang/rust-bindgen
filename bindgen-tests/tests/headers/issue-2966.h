901a4a002673ce1901c10c8951638f15361ee98b
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2024-10-30 22:42:38 -0400

    Use `field_visibility` callback for new-type aliases
    The `field_visibility` callback is now called in case of alias new-type and new-type-deref with the type name and field_name set to `"0"`

---
+++ bindgen-tests/tests/headers/issue-2966.h
@@ -0,0 +1,6 @@
+// bindgen-flags: --default-alias-style=new_type
+// bindgen-parse-callbacks: type-visibility
+
+typedef const char *  pub_var1;
+typedef const char *  pubcrate_var2;
+typedef const char *  private_var3;
