3ac14ed5442bebc1c5687ce909ecd1560f5499fc
Christian Poveda <git@pvdrz.com>
2024-11-27 14:17:23 -0500

    Ignore the `layout.h` test
    This is done because bindgen is producing bogus code where a single
    struct has both `packed` and `align` attributes.

--- bindgen-tests/tests/headers/layout.h
+++ bindgen-tests/tests/headers/layout.h
@@ -2,9 +2,12 @@
 //
 // FIXME: https://github.com/rust-lang/rust-bindgen/issues/1498
 
+
+#if 0
 struct header
 {
     char proto;
     unsigned int size __attribute__ ((packed));
     unsigned char data[] __attribute__ ((aligned(8)));
 } __attribute__ ((aligned, packed));
+#endif
