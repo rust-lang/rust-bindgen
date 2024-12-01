a478fd19565468e93a726945aa1d839061891c4d
Christian Poveda <git@pvdrz.com>
2024-11-30 20:24:27 -0500

    Update the `--merge-extern-blocks` tests

---
+++ bindgen-tests/tests/headers/merge_extern_blocks_post_1_82.hpp
@@ -0,0 +1,14 @@
+// bindgen-flags: --merge-extern-blocks --enable-cxx-namespaces --rust-target=1.82 -- --target=x86_64-unknown-linux
+int foo();
+typedef struct Point {
+    int x;
+} Point;
+int bar();
+
+namespace ns {
+    int foo();
+    typedef struct Point {
+        int x;
+    } Point;
+    int bar();
+}
