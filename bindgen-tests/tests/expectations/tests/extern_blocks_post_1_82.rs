be9b4b9dcf7e34506eee45dfbd73a6e82a98286d
Christian Poveda <git@pvdrz.com>
2024-11-30 20:24:27 -0500

    Test the `unsafe_extern_blocks` feature

---
+++ bindgen-tests/tests/expectations/tests/extern_blocks_post_1_82.rs
@@ -0,0 +1,7 @@
+#![allow(dead_code, non_snake_case, non_camel_case_types, non_upper_case_globals)]
+unsafe extern "C" {
+    pub fn cool_function(i: ::std::os::raw::c_int, c: ::std::os::raw::c_char);
+}
+unsafe extern "C" {
+    pub static mut cool_static: ::std::os::raw::c_int;
+}
---
+++ bindgen-tests/tests/headers/extern_blocks_pre_1_82.h
@@ -0,0 +1,5 @@
+// bindgen-flags: --no-layout-tests --rust-target=1.81
+
+void cool_function(int i, char c);
+
+static int cool_static;
