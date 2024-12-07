rust-lang/rust-bindgen

commit 713dbdab8a588f107936bd3e8d83288130d4b498

Handle version and shell completions

bindgen/options/cli.rs
@@ -14,11 +14,10 @@ use clap::{
     CommandFactory, Parser,
 };
 use proc_macro2::TokenStream;
-use std::fs::File;
 use std::io;
 use std::path::{Path, PathBuf};
-use std::process::exit;
 use std::str::FromStr;
+use std::{fs::File, process::exit};
 
 fn rust_target_help() -> String {
     format!(
@@ -649,28 +648,6 @@ where
         clang_args,
     } = command;
 
-    if let Some(shell) = generate_shell_completions {
-        clap_complete::generate(
-            shell,
-            &mut BindgenCommand::command(),
-            "bindgen",
-            &mut std::io::stdout(),
-        );
-
-        exit(0);
-    }
-
-    if version {
-        println!(
-            "bindgen {}",
-            option_env!("CARGO_PKG_VERSION").unwrap_or("unknown")
-        );
-        if verbose {
-            println!("Clang: {}", crate::clang_version().full);
-        }
-        std::process::exit(0);
-    }
-
     let mut builder = builder();
 
     #[derive(Debug)]
@@ -822,6 +799,27 @@ where
 
     builder = apply_args!(
         builder {
+            generate_shell_completions => |_, shell| {
+                clap_complete::generate(
+                    shell,
+                    &mut BindgenCommand::command(),
+                    "bindgen",
+                    &mut std::io::stdout(),
+                );
+
+                exit(0)
+            },
+            version => |_, _| {
+                println!(
+                    "bindgen {}",
+                    option_env!("CARGO_PKG_VERSION").unwrap_or("unknown")
+                );
+                if verbose {
+                    println!("Clang: {}", crate::clang_version().full);
+                }
+
+                exit(0)
+            },
             header,
             rust_target,
             default_enum_style,

commit 2e154e7e2af2f5c40f195c3ec5b468ae2259c1c7

Unbreak shell completion and --version without header
This regressed again in #2984. Partially revert d75fe271418e and
42a86e288c43 and restore the previous behavior.

Fixes: https://github.com/rust-lang/rust-bindgen/issues/3037
Fixes: https://github.com/rust-lang/rust-bindgen/issues/3039

bindgen/options/cli.rs
@@ -146,7 +146,7 @@ fn parse_custom_attribute(
 #[allow(clippy::doc_markdown)]
 struct BindgenCommand {
     /// C or C++ header file.
-    header: String,
+    header: Option<String>,
     /// Path to write depfile to.
     #[arg(long)]
     depfile: Option<String>,
@@ -657,6 +657,33 @@ where
         clang_args,
     } = command;
 
+    if let Some(shell) = generate_shell_completions {
+        clap_complete::generate(
+            shell,
+            &mut BindgenCommand::command(),
+            "bindgen",
+            &mut io::stdout(),
+        );
+
+        exit(0)
+    }
+
+    if version {
+        println!(
+            "bindgen {}",
+            option_env!("CARGO_PKG_VERSION").unwrap_or("unknown")
+        );
+        if verbose {
+            println!("Clang: {}", crate::clang_version().full);
+        }
+
+        exit(0)
+    }
+
+    if header.is_none() {
+        return Err(io::Error::new(io::ErrorKind::Other, "Header not found"));
+    }
+
     let mut builder = builder();
 
     #[derive(Debug)]
@@ -804,31 +831,8 @@ where
         }
     }
 
-    let header = Some(header);
-
     builder = apply_args!(
         builder {
-            generate_shell_completions => |_, shell| {
-                clap_complete::generate(
-                    shell,
-                    &mut BindgenCommand::command(),
-                    "bindgen",
-                    &mut io::stdout(),
-                );
-
-                exit(0)
-            },
-            version => |_, _| {
-                println!(
-                    "bindgen {}",
-                    option_env!("CARGO_PKG_VERSION").unwrap_or("unknown")
-                );
-                if verbose {
-                    println!("Clang: {}", crate::clang_version().full);
-                }
-
-                exit(0)
-            },
             header,
             rust_target,
             rust_edition,