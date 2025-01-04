4ea949f742c13f9529259f639234583c3bfe22c6
Jonathan Schwender <schwenderjonathan@gmail.com>
2025-01-04 14:49:47 +0100

    Allow blocklisting anonymous enums
    Note: This will also block anonymous enums with no variants, but
    adding a typedef for such an enum should be useless anyway.

--- bindgen-tests/tests/headers/issue-1025-unknown-enum-repr.hpp
+++ bindgen-tests/tests/headers/issue-1025-unknown-enum-repr.hpp
@@ -2,3 +2,19 @@
 template <typename> class a {
   enum {};
 };
+
+template <typename> class b {
+  enum {
+    SOME_VARIANT
+  };
+};
+
+template <typename> class c {
+  enum my_enum {
+    MY_ENUM_SOME_VARIANT
+  };
+};
+
+template <typename> class d {
+  enum no_variant_enum {};
+};
