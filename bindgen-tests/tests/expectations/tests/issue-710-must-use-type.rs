9c02ec5de546243d968c283425fedcf9a473cf94
oberrich <oberrich@img.ws>
2025-02-06 14:11:17 +0100

    fix(tests): apply new `#[must_use]` order

--- bindgen-tests/tests/expectations/tests/issue-710-must-use-type.rs
+++ bindgen-tests/tests/expectations/tests/issue-710-must-use-type.rs
@@ -1,14 +1,14 @@
 #![allow(dead_code, non_snake_case, non_camel_case_types, non_upper_case_globals)]
+#[must_use]
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
-#[must_use]
 pub struct A {
     _unused: [u8; 0],
 }
 /// <div rustbindgen mustusetype></div>
+#[must_use]
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
-#[must_use]
 pub struct B {
     _unused: [u8; 0],
 }
