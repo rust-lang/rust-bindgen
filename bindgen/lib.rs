e2940cdcc20967799bece79b46682e8acc3bc042
Emilio Cobos Álvarez <emilio@crisal.io>
2025-06-08 12:13:24 +0200

    chore: Fix some warnings with latest stable.

--- bindgen/lib.rs
+++ bindgen/lib.rs
@@ -980,7 +980,7 @@ impl Bindings {
     }
 
     /// Gets the rustfmt path to rustfmt the generated bindings.
-    fn rustfmt_path(&self) -> io::Result<Cow<PathBuf>> {
+    fn rustfmt_path(&self) -> io::Result<Cow<'_, PathBuf>> {
         debug_assert!(matches!(self.options.formatter, Formatter::Rustfmt));
         if let Some(ref p) = self.options.rustfmt_path {
             return Ok(Cow::Borrowed(p));
