3d2410e2b06d7349a688b6cb1b8ce49d5d941ffa
Miguel Ojeda <ojeda@kernel.org>
2025-08-22 11:20:04 +0200

    ci: Move forward Rust for Linux version to v6.17-rc2
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

--- ci/test.sh
+++ ci/test.sh
@@ -96,7 +96,7 @@ if [ "$BINDGEN_RUST_FOR_LINUX_TEST" == "1" ]; then
   # and each update should only contain this change.
   #
   # Both commit hashes and tags are supported.
-  LINUX_VERSION=v6.16-rc1
+  LINUX_VERSION=v6.17-rc2
 
   # Download Linux at a specific commit
   mkdir -p linux
