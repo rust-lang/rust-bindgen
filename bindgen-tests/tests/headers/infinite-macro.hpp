bd1da850e5201fc2cad24d903088150c9d6edaa7
Nico Burns <nico@nicoburns.com>
2025-10-02 01:04:35 +0100

    Add tests for f32 literals

---
+++ bindgen-tests/tests/headers/infinite-macro.hpp
@@ -0,0 +1,7 @@
+#define INFINITY (1.0f/0.0f)
+#define NEG_INFINITY (-1.0f/0.0f)
+#define NAN (0.0f/0.0f)
+
+static const float F32_INFINITY = 1.0f / 0.0f;
+static const float F32_NEG_INFINITY = -1.0f / 0.0f;
+static const float F32_NAN = 0.0f / 0.0f;
