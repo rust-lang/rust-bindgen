0d367958c011fd45910e4dab741ac1b6acf1417e
Armoha <kein0011@naver.com>
2026-07-07 10:51:30 +0900

    Add test case for issue #3303

---
+++ bindgen-tests/tests/expectations/tests/issue-3303-new-type-alias-default.rs
@@ -0,0 +1,2 @@
+#![allow(dead_code, non_snake_case, non_camel_case_types, non_upper_case_globals)]
+pub const BOGUS_CONST: u32 = u32(0);
---
+++ bindgen-tests/tests/headers/issue-3303-new-type-alias-default.h
@@ -0,0 +1,3 @@
+// bindgen-flags: --default-alias-style=new_type
+
+#define BOGUS_CONST 0x00000000
