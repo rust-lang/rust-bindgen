239dc12d83d7e68d6e1158a1672a672b967d0f5b
Per Larsen <perlarsen@google.com>
2026-08-30 03:39:47 +0200

    Test edge-case values for repr(C) rustified enums
    Cover an enum value in the i32::MAX+1..=u32::MAX range, an enum mixing
    negative and positive values, and one whose mixed-sign values only fit
    in 64 bits. The case triggers rustc's repr_c_enums_larger_than_int
    future-compatibility lint.
    
    Signed-off-by: Per Larsen <perlarsen@google.com>

--- bindgen-tests/tests/expectations/tests/enums-repr-c.rs
+++ bindgen-tests/tests/expectations/tests/enums-repr-c.rs
@@ -29,3 +29,39 @@ const _: () = {
     ["Size of large_value_t"][::std::mem::size_of::<large_value_t>() - 4usize];
     ["Alignment of large_value_t"][::std::mem::align_of::<large_value_t>() - 4usize];
 };
+#[repr(C)]
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
+pub enum unsigned_value_t {
+    UNSIGNED_VALUE = 2147483648,
+}
+#[allow(clippy::unnecessary_operation, clippy::identity_op)]
+const _: () = {
+    ["Size of unsigned_value_t"][::std::mem::size_of::<unsigned_value_t>() - 4usize];
+    [
+        "Alignment of unsigned_value_t",
+    ][::std::mem::align_of::<unsigned_value_t>() - 4usize];
+};
+#[repr(C)]
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
+pub enum mixed_sign_t {
+    NEGATIVE_VALUE = -1,
+    POSITIVE_VALUE = 1,
+}
+#[allow(clippy::unnecessary_operation, clippy::identity_op)]
+const _: () = {
+    ["Size of mixed_sign_t"][::std::mem::size_of::<mixed_sign_t>() - 4usize];
+    ["Alignment of mixed_sign_t"][::std::mem::align_of::<mixed_sign_t>() - 4usize];
+};
+#[repr(C)]
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq)]
+pub enum wide_mixed_sign_t {
+    WIDE_NEGATIVE_VALUE = -1,
+    WIDE_UNSIGNED_VALUE = 2147483648,
+}
+#[allow(clippy::unnecessary_operation, clippy::identity_op)]
+const _: () = {
+    ["Size of wide_mixed_sign_t"][::std::mem::size_of::<wide_mixed_sign_t>() - 8usize];
+    [
+        "Alignment of wide_mixed_sign_t",
+    ][::std::mem::align_of::<wide_mixed_sign_t>() - 8usize];
+};
--- bindgen-tests/tests/headers/enums-repr-c.hpp
+++ bindgen-tests/tests/headers/enums-repr-c.hpp
@@ -17,3 +17,23 @@ typedef enum {
 } large_value_t;
 
 static_assert(sizeof(large_value_t) == 4, "");
+
+typedef enum {
+    UNSIGNED_VALUE = 0x80000000,
+} unsigned_value_t;
+
+static_assert(sizeof(unsigned_value_t) == 4, "");
+
+typedef enum {
+    NEGATIVE_VALUE = -1,
+    POSITIVE_VALUE = 1,
+} mixed_sign_t;
+
+static_assert(sizeof(mixed_sign_t) == 4, "");
+
+typedef enum {
+    WIDE_NEGATIVE_VALUE = -1,
+    WIDE_UNSIGNED_VALUE = 0x80000000,
+} wide_mixed_sign_t;
+
+static_assert(sizeof(wide_mixed_sign_t) == 8, "");
